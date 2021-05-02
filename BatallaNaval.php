c6ea217100d73841473f8c840df74180543ae5ac
Luna-Ghost <83356101+Luna-Ghost@users.noreply.github.com>
2021-05-02 16:11:48 -0500

    FEAT: php e imagenes

---
+++ BatallaNaval.php
@@ -0,0 +1,88 @@
+<?php
+    echo "<h1><i>Batalla Naval</i></h1>";
+    $cx=1;
+    $cy="A";
+    $vidas=8;
+    $historial="";
+    $pantalla=0;
+    $casilla_y =    [
+                        1 => "A",
+                        2 => "B",
+                        3 => "C",
+                        4 => "D",
+                        5 => "E",
+                        6 => "F",
+                        7 => "G",
+                        8 => "H",
+                        9 => "I",
+                        10 => "J"
+    ];
+    echo "<h3>Vidas: </h3>";
+    
+    while($vidas>0)
+    {
+        for($i=1; $i<=$vidas; $i++)
+        {
+            echo "<img src='https://png.pngtree.com/png-vector/20191008/ourlarge/pngtree-bullet-icon-in-cartoon-style-png-image_1799886.jpg' alt='bala' height='20'>";
+        }
+        //codigo de historial
+        echo "<br><br>";
+        echo "Historial: <br>";
+        echo $historial;
+        echo "<br><br>";
+        echo "<table border='1'>";
+            echo "<thead>";
+                echo "<tr>";
+                    echo "<th></th>";
+                    for ($x=1; $x<=10; $x++)
+                    {
+                        echo "<th>";
+                        echo $x;
+                        echo "</th>";
+                    }
+                echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+                for($y=1; $y<=10; $y++)
+                {
+                    echo "<tr>";
+                        echo "<th>";
+                            foreach ($casilla_y as $num => $letra) 
+                            {
+                                if($y==$num)
+                                {
+                                    echo $letra;
+                                }
+                            }
+                        echo "</th>";
+                        for($cuadro=1; $cuadro<=10; $cuadro++)
+                        {
+                            echo "<th>";
+                                switch ($pantalla)
+                                {
+                                    case 0:
+                                        echo "<img src='./agua.png' alt='imagen de agua' height='25'>";
+                                        break;
+                                    case 1:
+                                        break;
+                                    case 2:
+                                        break;
+                                }
+                            echo "</th>";
+                        }
+                    echo "</tr>";
+                }
+                
+            echo "</tbody>";
+        echo "</table>";
+        echo "<form action='./BatallaNaval.php'>";
+            echo "Coordenada X(numero): <input type='number' name='numero' min='0', max='10' required>";
+            echo "Coordenada Y(letra): <input type='text' name='letra' required>";
+            echo "<input type='submit' value='Dispara!!!!'>";
+        echo "</form>"
+        $vidas-=1;
+    }
+    echo "<h1><i>¡¡¡PERDISTE!!!</i></h1>";
+    echo "<h2><i>Has perdido todas tus vidas</i></h2>";
+    
+?>
